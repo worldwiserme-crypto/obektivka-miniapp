dc4318d17595c72d8c24d179df96826df802313c
worldwiserme-crypto <worldwiser.me@gmail.com>
2026-03-08 07:09:56 +0500

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Node.js yo'lini topish
-export PATH="/nix/var/nix/profiles/default/bin:/usr/local/bin:/usr/bin:/bin:$PATH"
-echo "Node: $(which node)"
-echo "Node version: $(node --version)"
-echo "Files: $(ls /app/)"
-python bot.py
